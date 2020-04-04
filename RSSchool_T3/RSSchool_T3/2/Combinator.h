ca4f9d6b4eb4b39029e2fc7cf85fcda2b9cc761f
WolfandWolf <antonos374@GMAIL.COM>
2020-04-04 13:04:24 +0300

    feat: add ex2

--- RSSchool_T3/RSSchool_T3/2/Combinator.h
+++ RSSchool_T3/RSSchool_T3/2/Combinator.h
@@ -1,5 +1,9 @@
 #import <Foundation/Foundation.h>
 
+@interface Math : NSObject
++ (long double)Factorial:(int)number;
+@end
+
 @interface Combinator : NSObject
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array;
 @end
