6c74f55ac8d5d115b40d7f14e0229df8f433f2de
WolfandWolf <antonos374@GMAIL.COM>
2020-04-04 13:51:17 +0300

    feat: add ex1

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -2,6 +2,53 @@
 
 @implementation PolynomialConverter
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+    NSMutableString *result = [[NSMutableString alloc] init];
+    unsigned long int degree = [numbers count] - 1;
+    if ([numbers isEqualToArray:@[]])
+        return nil;
+    for (NSNumber *counter in numbers) {
+        if ([counter intValue] > 1 && degree != 0)
+            [result appendFormat:@"%dx", [counter intValue]];
+        else if ([counter intValue] < -1 && degree != 0)
+            [result appendFormat:@"%dx", -[counter intValue]];
+        else if ([counter intValue] == 0) {
+            degree--;
+            continue;
+        }
+        else if ([counter intValue] == 1 || [counter intValue] == -1)
+            [result appendFormat:@"x"];
+        
+        if (degree > 1) {
+            [result appendFormat:@"^%d", (int)degree];
+            degree--;
+            if ([[numbers objectAtIndex:([numbers count] - degree - 1)] intValue] >= 0) {
+                [result appendString:@" + "];
+                continue;
+            }
+            else {
+                [result appendString:@" - "];
+                continue;
+            }
+        }
+        else if (degree == 1) {
+            degree--;
+            if ([[numbers objectAtIndex:([numbers count] - degree - 1)] intValue] > 0) {
+                [result appendString:@" + "];
+                continue;
+            }
+            else if ([[numbers objectAtIndex:([numbers count] - degree - 1)] intValue] == 0) {
+                return result;
+            }
+            else {
+                [result appendString:@" - "];
+                continue;
+            }
+        }
+        else if (degree == 0) {
+            [result appendFormat:@"%d", [counter intValue]];
+            return result;
+        }
+    }
+    return result;
 }
 @end
