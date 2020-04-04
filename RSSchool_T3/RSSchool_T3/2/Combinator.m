59624f175ac1029d0e636d81c52e93ba638622d0
WolfandWolf <antonos374@GMAIL.COM>
2020-04-04 13:04:24 +0300

    feat: add ex2

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -1,7 +1,19 @@
 #import "Combinator.h"
 
+@implementation Math : NSObject
++ (long double)Factorial:(int)number {
+    if (number == 0)
+        return 1;
+    return number * [Math Factorial:number - 1];
+}
+@end
+
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    for (int counter = 1; counter < [[array objectAtIndex:1] intValue]; counter++) {
+        if ([Math Factorial:([[array objectAtIndex:1] intValue])]/ ([Math Factorial:([[array objectAtIndex:1] intValue] - counter)] * [Math Factorial:(counter)]) == [[array objectAtIndex:0] intValue])
+            return [NSNumber numberWithInt:counter];
+    }
+    return nil;
 }
 @end
